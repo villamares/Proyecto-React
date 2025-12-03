133510b78b7b6b1fdb85edff41f0c518da8d37b3
Adrian Villamares <villamares@gmail.com>
2025-12-03 08:48:48 -0600

    Crear el primer componente

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import PrimerComponente from './components/PrimerComponente';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        
+        <PrimerComponente />
       </header>
     </div>
   );
---
+++ src/components/PrimerComponente.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const PrimerComponente = () => {
+  return (
+    <div>
+        <h1>Hola, soy el primer componente</h1>
+        <h2>Estoy aprendiendo React</h2>
+        <p>Este es mi primer componente funcional</p>
+    </div>
+  )
+}
+
+export default PrimerComponente
